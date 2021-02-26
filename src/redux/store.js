dd2aaec4fedc6c299918f312a9476331fd6d9842
Michael Matos <mbaezmichael@gmail.com>
2021-02-25 22:14:20 -0500

    redux/react devtools (+)

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,24 +2,14 @@ import { createStore } from "redux";
 
 import reducer from "./reducers";
 
-// export default function configureStore(initialState) {
-//   const store = createStore(
-//     reducer,
-//     initialState,
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   );
-
-
-
-//   return store;
-// }
 export default function configureStore(initialState) {
   const store = createStore(
     reducer,
-    initialState
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
-  
+
 
   return store;
 }
